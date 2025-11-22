1843224c04dcb7e3336a975cd8f22a89d3a3b726
Landro Capul <landro_capul@sjp2cd.edu.ph>
2025-11-22 21:27:56 +0800

    s

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE lazada CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE lazada;
+-- CREATE DATABASE lazada CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- USE lazada;
 
 -- Users table
 CREATE TABLE users (
